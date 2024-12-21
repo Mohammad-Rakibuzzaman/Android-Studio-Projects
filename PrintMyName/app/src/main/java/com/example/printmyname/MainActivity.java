2c97b6bcefdb76f4bc0d628091465aa2143128b4
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-12-21 21:15:03 +0600

    add if else condition and show final output in the main screen. Successfully learn how to add button text and input field in xml and how to make interactive each thing. Finaloutput was write a name and show it by printing button. also it will show default name aswell.

--- PrintMyName/app/src/main/java/com/example/printmyname/MainActivity.java
+++ PrintMyName/app/src/main/java/com/example/printmyname/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.printmyname;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +13,39 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button showButton;
+    private TextView nameText;
+
+    private EditText enterName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        showButton = findViewById(R.id.button);
+        nameText = findViewById(R.id.textView);
+        enterName = findViewById(R.id.editTextName);
+
+//        nameText.setText("Hello from TechnexPLC!");
+
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = enterName.getText().toString();
+                if (name.isEmpty()) {
+                    nameText.setText("Hello, RakibRatul! :)");
+                } else {
+                    nameText.setText("Hello, " + name + "!!! :))");
+                }
+
+
+            }
+        });
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
